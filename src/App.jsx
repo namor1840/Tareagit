65bfec1a9c68ffc04098a9ccb69a7d427d301637
Roman Franco <namor18@gmail.com>
2023-06-16 22:35:47 -0400

    segundo commit, corrigiendo error

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ function App() {
 
   return (
 
-    <div>
-      <img src='https://picsum.photos/200' alt='Imagen'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi amet, velit fugit assumenda eos dolorem autem quo repudiandae dolorum temporibus error optio accusamus molestiae. Illum consequatur numquam odit temporibus cupiditate.
-      </img>
-    </div>
-  )
+    <><div>
+      <img src='https://picsum.photos/200' alt='Imagen' />
+
+    </div></>
+
+
+
+       
+
+)
 }
 
 export default App
